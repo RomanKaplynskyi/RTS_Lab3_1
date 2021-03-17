4b863564c31b720f9a2c0e654a9583d29904f916
zayaz <zayaz20022001@gmail.com>
2021-03-17 20:07:47 +0200

    Lab3_1 Main Part

---
+++ main.py
@@ -0,0 +1,7 @@
+# import numpy                                    # Для проведення математичних операцій
+from factorization import methodFerma
+
+n = int(input("input number for factorization: "))
+methodFerma(n)
+
+
